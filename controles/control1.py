7a072f87ab1c2477fd517af1f1abe8c02ca0ed11
peotrasher <felipe.escobar018@gmail.com>
2023-03-29 12:54:12 -0300

    Añadi dos directorios mas, alguna correccion de control1.py

--- controles/control1.py
+++ controles/control1.py
@@ -10,7 +10,7 @@
 # strings estan en minusculas y sin tilde.
 #
 # Conclusiones:
-# (1) Necesito una ~funcion~ filtra
+# (1) Necesito una ~funcion filtra~
 #  |_ (2) RECIBE una ~lista L de strings~.
 #  |_ (3) RECIBE una ~string de referencia~.
 #  |_ (4) RETORNA una ~nueva lista con aquellas strings con un 
@@ -26,23 +26,27 @@
 def filtra(L: list, ref: str) -> list:
     # Creo mi nueva lista que RETORNARE.
     L_resultado = []
+    # Pero ahora necesito una forma de contar las vocales de las 
+    # strings que estoy recorriendo en la ~lista L~ para asi 
+    # compararlas con aquellas de la ~string de referencia~.
+    #
+    # | Explicacion mas abajito |
+    #
+    # Listo, ahora guardo en una variable el numero de vocales en la
+    # ~string de referencia~.
+    vocales_str_de_ref = cuenta_vocales(ref)
     # Recorro la ~lista L de strings~.
     for i in L:
-        # Pero necesito una forma de contar las vocales de las strings
-        # que estoy recorriendo en la ~lista L~ para asi compararlas
-        # con aquellas de la ~string de referencia~.
-        #
-        # | Explicacion mas abajito |
-        #
-        # Tengo la forma, ahora comparo el numero de vocales que tengo
-        # en la string en el indice i de la lista L con el numero que 
-        # tengo en la string ref, evaluo si el numero de i es mayor o 
+        # Comparo el numero de vocales que tengo en la string ubicada 
+        # en el indice i de la lista L con el numero que tengo en la 
+        # string ref (ya contabilizadas en la variable 
+        # "vocales_str_de_ref"), evaluo si el numero de i es mayor o 
         # igual que el de ref.
-        if cuenta_vocales(i) >= cuenta_vocales(ref):
-            # ¿ES MAYOR O IGUAL? ... SI
+        if cuenta_vocales(i) >= vocales_str_de_ref:
+            # ¿¿¿ES MAYOR O IGUAL??? ... SI
             # Agrego a L_resultado la string de L con la que hice la
-            # comparacion (L[i]).
-            # ¿NO LO ES? ... NO
+            # comparacion ("L[i]").
+            # ¿¿¿NO LO ES??? ... NO
             # No hago nada.
             L_resultado.append(i)
     return L_resultado
@@ -59,17 +63,17 @@ def cuenta_vocales(string: str) -> int:
     return string.count('a') + string.count('e') + string.count('i') \
            + string.count('o') + string.count('u')
     # Otra forma de hacer este algoritmo pudiese ser:
-    """
-    vocales = "aeiou"          
-    cantidad_de_vocales = 0     
-    for i in range(0, len(string)):
-        for j in range(0, len(vocales)):
-            if string[i] == vocales[j]:
-                cantidad_de_vocales += 1
-    return cantidad_de_vocales
-    """
+    #
+    #           vocales = "aeiou"          
+    #           cantidad_de_vocales = 0     
+    #           for i in range(0, len(string)):
+    #               for j in range(0, len(vocales)):
+    #                    if string[i] == vocales[j]:
+    #                        cantidad_de_vocales += 1
+    #           return cantidad_de_vocales
+    #
     # Pero hay multiples formas en realidad, algunas mas o menos
-    # pythonescas, la mostrada es menos.
+    # pythonescas, la mostrada lo es menos.
     
 
 def main():
