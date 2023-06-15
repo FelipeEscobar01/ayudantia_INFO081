502a316843027d1dfb438706bd5683918a0685a6
peotrasher <felipe.escobar018@gmail.com>
2023-06-15 18:31:46 -0400

    No se

--- 10-introduccion_cpp/ejercicio1.cpp
+++ 10-introduccion_cpp/ejercicio1.cpp
@@ -8,15 +8,8 @@ int main(){
     int mediana, i;
     cout << "Ingrese palabra: "; cin >> palabra;
     mediana = palabra.length() / 2;
-    if(!(mediana % 2)){
-        for(i = 0; i < mediana; i++)
-            swap(palabra[i], palabra[(palabra.length() - i) - 1]);
-    }
-    else{
-        i = 0;
-        while(i != mediana + 1)
-            swap(palabra[i], palabra[(palabra.length() - i) - 1]);
-    }
+    for (i = 0; i < mediana; i++)
+        swap(palabra[i], palabra[(palabra.length() - i) - 1]);
     cout << palabra << endl;
     return EXIT_SUCCESS;
 }
