09824f7a81f0ec0705da6ccf4d583bc77dd32f46
peotrasher <felipe.escobar018@gmail.com>
2023-07-06 13:32:45 -0400

    Plantilla ejercicio 5

---
+++ 13-susti_repaso/plantilla_ej5.py
@@ -0,0 +1,24 @@
+from random import *
+
+L_MIN = 5; L_MAX = 20; N_MIN = 0; N_MAX = 50
+
+def genera_ejercicio(size: int) -> tuple:
+    lista = []
+    l_size = 0
+    while (l_size != size - 1):
+        lista.append(randint(N_MIN, N_MAX))
+        l_size += 1
+    pos = randint(0, size - 1)
+    lista.append(lista[pos])
+    return (lista, lista[pos] * 2)
+
+def two_sum(lista: list, suma: int) -> dict:
+    pass
+
+def main():
+    ejercicio = genera_ejercicio(randint(L_MIN, L_MAX))
+    print(ejercicio)
+    print(two_sum(ejercicio[0], ejercicio[1]))
+
+if __name__ == "__main__":
+    main()
